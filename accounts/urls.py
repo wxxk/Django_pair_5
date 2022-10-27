cc83af5a9444b15905f2fd8b70a00242996050ad
dongwonbaek <bmrio100@gmail.com>
2022-10-27 14:31:43 +0900

    accounts 기능 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 app_name = "accounts"
 
-urlpatterns = []
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('<int:pk>/', views.detail, name='detail'),
+    path('update/', views.update, name='update'),
+    path('password/', views.password_update, name='password_update'),
+    path('delete/', views.delete, name='delete'),
+    path('<int:pk>/follow/', views.follow, name='follow'),
+]
